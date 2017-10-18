45cf72ffe3da688bea916f98a78e8c530d1f0c10
Kevin Mendoza <kmendoza4@ucmerced.edu>
2017-10-18 14:14:09 -0600

    sean halp
    sean halp alot

--- src/root/gui/gui_main.py
+++ src/root/gui/gui_main.py
@@ -1,9 +1,13 @@
 import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 
 kivy.require("1.10.0")
+
+class PongGame(Widget):
+    pass
 
 class GuiMainApp(App):
     
@@ -11,7 +15,9 @@ class GuiMainApp(App):
         super().__init__(**kwargs)
         self.controller = controller
         
-        ''' def build(self):
+    def build(self):
+        return PongGame()
+        '''
         label = Label(text="SEAN")
         layout = BoxLayout()
         layout.add_widget(label, 0)
